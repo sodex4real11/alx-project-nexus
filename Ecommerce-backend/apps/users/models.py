00dccf8604d37736b8d299959d2711e41f193a4d
sodex4real11 <sodex4real11@gmail.com>
2025-11-30 06:03:11 +0700

    new

--- Ecommerce-backend/apps/users/models.py
+++ Ecommerce-backend/apps/users/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'users'
+        indexes = [
+            models.Index(fields=['email']),
+            models.Index(fields=['username']),
+        ]
+
+    def __str__(self):
+        return self.email
